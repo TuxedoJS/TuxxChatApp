cf4c7447064295841cbb2a3c5f768fa22805aeac
drabinowitz <dmitrirabinowitz+development@gmail.com>
2015-01-20 10:56:57 -0500

    Refactor messageActions and Stores to use before syntax.

--- client/app/actions/MessageActions.js
+++ client/app/actions/MessageActions.js
@@ -1,11 +1,65 @@
 'use strict';
 
+var $ = require('../../helpers/AjaxHelper');
 var Actions = require('tux/Actions');
 
 var MessageActions = Actions.createActionCategory({
   category: 'message',
   source: 'message_views',
   actions: ['get', 'create', 'destroy', 'update']
+});
+
+MessageActions.before('get', function (dispatch, actionBody) {
+  $.ajax({
+    type: 'GET',
+    location: 'messages',
+    data: actionBody
+  })
+  .then(function (data) {
+    dispatch(data);
+  });
+});
+
+MessageActions.before('create', function (dispatch, actionBody) {
+  $.ajax({
+    type: 'POST',
+    location: 'messages',
+    data: actionBody
+  })
+  .then(function (data) {
+    dispatch(data);
+  });
+});
+
+MessageActions.before('destroy', function (dispatch, actionBody) {
+  $.ajax({
+    type: 'DELETE',
+    location: 'messages',
+    data: {
+      messageId: actionBody.id,
+      roomId: actionBody.roomId
+    }
+  })
+  .then(function () {
+    dispatch({
+      id: actionBody.id
+    });
+  });
+});
+
+MessageActions.before('update', function (dispatch, actionBody) {
+  $.ajax({
+    type: 'PUT',
+    location: 'messages',
+    data: {
+      text: actionBody.text,
+      roomId: actionBody.roomId,
+      messageId: actionBody.id
+    }
+  })
+  .then(function (data) {
+    dispatch(data);
+  });
 });
 
 module.exports = MessageActions;
--- client/app/stores/MessageStore.js
+++ client/app/stores/MessageStore.js
@@ -1,101 +1,53 @@
 'use strict';
 
-var $ = require('../../helpers/AjaxHelper');
-var Stores = require('tux/Stores');
-var MessageActions = require('../actions/MessageActions');
+var ActionStores = require('tux/Stores/ActionStores');
 
-var MessageStore = Stores.createStore({
+var MessageStore = ActionStores.createStore({
   _messages: [],
 
   all: function () {
     return this._messages;
   },
 
-  get: function (roomId) {
-    $.ajax({
-      type: 'GET',
-      data: { roomId: roomId },
-      location: 'messages'
-    })
-      .then(function (data) {
-        this._messages = data;
-        this.emitChange();
-      }.bind(this));
+  onGet: function (data) {
+    this._messages = data;
+    this.emitChange();
   },
 
-  create: function (text, roomId, username) {
-    $.ajax({
-      type: 'POST',
-      location: 'messages',
-      data: {
-        text: text,
-        roomId: roomId,
-        username: username
+  onCreate: function (data) {
+    this._messages.push(data);
+    this.emitChange();
+  },
+
+  onDestroy: function (data) {
+    for (var i = 0; i < this._messages.length; i++) {
+      if (this._messages[i].id === data.id) {
+        this._messages.splice(i, 1);
+        break;
       }
-    })
-      .then( function(data) {
-        this._messages.push(data);
-        this.emitChange();
-      }.bind(this));
+    }
+    this.emitChange();
   },
 
-  destroy: function (id, roomId) {
-    $.ajax({
-      type: 'DELETE',
-      location: 'messages',
-      data: {
-        messageId: id,
-        roomId: roomId
+  onUpdate: function (data) {
+    for (var i = 0; i < this._messages.length; i++) {
+      if (this._messages[i].id === data.id) {
+        this._messages[i] = data;
+        break;
       }
-    })
-      .then(function (data) {
-        for (var i = 0; i < this._messages.length; i++) {
-          if (this._messages[i].id === id) {
-            this._messages.splice(i, 1);
-            break;
-          }
-        }
-        this.emitChange();
-      }.bind(this));
+    }
+    this.emitChange();
   },
 
-  update: function (text, roomId, messageId) {
-    $.ajax({
-      type: 'PUT',
-      location: 'messages',
-      data: {
-        text: text,
-        roomId: roomId,
-        messageId: messageId
+  register: function () {
+    return {
+      message: {
+        get: this.onGet,
+        create: this.onCreate,
+        destroy: this.onDestroy,
+        update: this.onUpdate
       }
-    })
-      .then(function (data) {
-        for (var i = 0; i < this._messages.length; i++) {
-          if (this._messages[i].id === data.id) {
-            this._messages[i] = data;
-            break;
-          }
-        }
-        this.emitChange();
-      }.bind(this));
-  },
-});
-
-MessageActions.register(MessageStore, {
-  get: function (body) {
-    MessageStore.get(body.roomId);
-  },
-
-  create: function (body) {
-    MessageStore.create(body.text, body.roomId, body.username);
-  },
-
-  destroy: function (body) {
-    MessageStore.destroy(body.id, body.roomId);
-  },
-
-  update: function (body) {
-    MessageStore.update(body.text, body.roomId, body.id);
+    };
   }
 });
 
